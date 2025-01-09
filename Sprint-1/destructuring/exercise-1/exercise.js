43a1f3bf78ccc4b8c02a10cd9640331998078772
bashirzadehdanial <bashirzadehdanial@gmail.com>
2025-01-09 23:50:56 +0000

    Update function to destructure the object

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,9 +6,9 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
-    `Hello, my name is ${personOne.name}. I am ${personOne.age} years old and my favourite food is ${personOne.favouriteFood}.`
+    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
