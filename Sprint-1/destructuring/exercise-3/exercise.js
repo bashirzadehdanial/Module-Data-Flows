0a420a528655514f32cf33b429957a6751fdb2f3
bashirzadehdanial <bashirzadehdanial@gmail.com>
2025-01-09 23:37:50 +0000

    Add receipt order

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,17 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+let total = 0;
+
+console.log("QTY     ITEM                TOTAL");
+
+order.forEach(({ quantity, itemName, unitPricePence }) => {
+  
+    const itemTotal = quantity * unitPricePence;
+    total += itemTotal;
+    console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${(itemTotal/100).toFixed(2)}`);
+});
+
+console.log(`\nTotal: ${(total/100).toFixed(2)}`);
